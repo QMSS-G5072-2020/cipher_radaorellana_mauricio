a5419a5583bbec1c1297355557d7cf119b7347b7
Mauricio <mro9395@gmail.com>
2020-11-15 14:58:41 -0500

    edit tests

--- tests/test_cipher_mer2245.py
+++ tests/test_cipher_mer2245.py
@@ -1,7 +1,6 @@
 from cipher_mer2245 import __version__
 from cipher_mer2245 import cipher_mer2245
 import pytest
-from cipher import cipher
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -11,5 +10,5 @@ def test_cipher_single():
     shift = 1
     encrypt = True
     expected = 'ifmmp'
-    actual = cipher(example,shift,encrypt)
+    actual = cipher_mer2245.cipher(example,shift,encrypt)
     assert actual == expected, 'Test not passed'
